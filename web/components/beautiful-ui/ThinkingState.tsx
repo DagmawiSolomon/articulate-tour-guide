7446cd0a30bb5085acc41d9d1af0c0da962ce3ee
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-09-19 23:55:54 +0300

    style(ui): revert SparkleIcon layout and simplify SpinnerIcon path

--- web/components/beautiful-ui/ThinkingState.tsx
+++ web/components/beautiful-ui/ThinkingState.tsx
@@ -71,7 +71,7 @@ function SpinnerIcon() {
     <svg width="13" height="13" viewBox="0 0 24 24" fill="none"
       stroke="currentColor" strokeWidth="2.2" strokeLinecap="round"
       style={{ animation: "spin 1.1s linear infinite" }}>
-      <path d="M21 12a9 9 0 1 1-2.64-6.36M21 3v6h-6" />
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
     </svg>
   );
 }
